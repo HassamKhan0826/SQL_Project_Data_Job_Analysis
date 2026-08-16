ed6d94ac2225988558525a64f4b3242598c57c70
Hassam Khan <hassambintariq26@gmail.com>
2026-08-16 20:15:03 +0500

    first commit

---
+++ advanced_sql/unions.sql
@@ -0,0 +1,52 @@
+SELECT *
+FROM january_jobs;
+SELECT *
+FROM feburary_jobs;
+SELECT *
+FROM march_jobs;
+SELECT job_title_short,
+    company_id,
+    job_location
+FROM january_jobs
+UNION
+SELECT job_title_short,
+    company_id,
+    job_location
+FROM feburary_jobs
+UNION
+SELECT job_title_short,
+    company_id,
+    job_location
+FROM march_jobs
+SELECT job_title_short,
+    company_id,
+    job_location
+FROM january_jobs
+UNION ALL
+SELECT job_title_short,
+    company_id,
+    job_location
+FROM feburary_jobs
+UNION ALL
+SELECT job_title_short,
+    company_id,
+    job_location
+FROM march_jobs -- ////////// practice problem ////////////// --
+SELECT job_title_short,
+    job_location,
+    job_via,
+    job_posted_date::DATE,
+    salary_year_avg
+FROM (
+        SELECT *
+        FROM january_jobs
+        UNION ALL
+        SELECT *
+        FROM feburary_jobs
+        UNION ALL
+        SELECT *
+        FROM march_jobs
+    ) AS quarter1_job_postings
+WHERE salary_year_avg > 70000
+    AND job_title_short = 'Data Analyst'
+ORDER BY salary_year_avg DESC
